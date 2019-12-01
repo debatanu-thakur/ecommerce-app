f2f20d2253066ca8b628dc343ddd8342261e7db2
debatanu-thakur <15355604+debatanu-thakur@users.noreply.github.com>
2019-12-01 13:05:33 +0100

    Add junit5 and gitignore to ignore system files

---
+++ src/test/java/com/example/demo/AuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class AuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
